46d4c0dc55cedb348448ccac32bcb0f7e4a60432
Ujjwal KC <kcujwal98@gmail.com>
2025-03-23 20:10:35 +0545

    Basic window

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,31 @@ extern crate sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
+
 pub fn main(){
 
     let sdl_contex = sdl2::init().unwrap();
     let video_subsystem = sdl_contex.video().unwrap();
 
-    let window = video_subsystem.window("Creating window", 1000, 700)
+    let window = video_subsystem.window("Creating window", 1800, 1000)
         .position_centered()
         .build()
         .unwrap();
 
     let mut event_pump = sdl_contex.event_pump().unwrap();
 
-    'running: loop{
+    'running: loop {
 
-        for event in event_pump.poll_iter(){
-            match event{
-                Event::Quit { .. } |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } =>{
-                    break 'running;
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
                 },
                 _ => {}
-
             }
         }
+
 
     }
 
