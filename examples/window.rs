a143df9c45d4fc408c51f0f57e8a108c9469a9ad
Ujjwal KC <kcujwal98@gmail.com>
2025-03-25 21:51:51 +0545

    Examples

---
+++ examples/window.rs
@@ -0,0 +1,34 @@
+extern crate sdl2;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
+
+pub fn main(){
+
+    let sdl_contex = sdl2::init().unwrap();
+    let video_subsystem = sdl_contex.video().unwrap();
+
+
+    let window = video_subsystem.window("First Window", 1200, 800)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut event_pump = sdl_contex.event_pump().unwrap();
+
+    'running: loop{
+
+        for event in event_pump.poll_iter(){
+            match event{
+                Event::Quit{..} |
+                Event::KeyDown {keycode: Some(Keycode::Escape), ..} =>{
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+
+    }
+
+}
